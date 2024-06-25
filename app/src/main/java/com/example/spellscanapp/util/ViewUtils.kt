624c2af05921225e3280a9084fc193a614e8e755
Murilo Bracero <murilobracero@gmail.com>
2024-06-25 14:12:14 -0300

    add profile screen

--- app/src/main/java/com/example/spellscanapp/util/ViewUtils.kt
+++ app/src/main/java/com/example/spellscanapp/util/ViewUtils.kt
@@ -2,6 +2,9 @@ package com.example.spellscanapp.util
 
 import android.view.View
 import android.view.ViewTreeObserver
+import android.widget.ImageView
+import coil.imageLoader
+import coil.request.ImageRequest
 
 inline fun View.afterMeasured(crossinline block: () -> Unit) {
     if (measuredWidth > 0 && measuredHeight > 0) {
@@ -17,4 +20,13 @@ inline fun View.afterMeasured(crossinline block: () -> Unit) {
             }
         })
     }
+}
+
+fun ImageView.loadFromUrl(url: String) {
+    val imageLoader = this.context.imageLoader
+    val request = ImageRequest.Builder(this.context)
+        .data(url)
+        .target(this)
+        .build()
+    imageLoader.enqueue(request)
 }
