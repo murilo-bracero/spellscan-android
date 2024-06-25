f26532c2056f697ce774c823e4192d406c2d6ce5
Murilo Bracero <50499575+murilo-bracero@users.noreply.github.com>
2024-06-25 09:23:52 -0300

    [APP] Refactor item scanning screen (#13)

--- app/src/main/java/com/example/spellscanapp/ui/fragment/CardAnalysisFragment.kt
+++ app/src/main/java/com/example/spellscanapp/ui/fragment/CardAnalysisFragment.kt
@@ -1,13 +1,31 @@
 package com.example.spellscanapp.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.spellscanapp.R
 import com.example.spellscanapp.databinding.FragmentCardAnalysisBinding
+import com.example.spellscanapp.logger.TAG
+import com.example.spellscanapp.ui.fragment.CardDetailFragment.Companion.ARG_CARD_ID
+import com.example.spellscanapp.ui.fragment.CardDetailFragment.Companion.ARG_HAS_CARD_FACES
+import com.example.spellscanapp.ui.viewmodel.CardServiceViewModel
+import com.example.spellscanapp.ui.viewmodel.CardViewModel
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.LENGTH_LONG
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
 
 class CardAnalysisFragment : Fragment() {
+
+    private val cardViewModel: CardViewModel by activityViewModels()
+    private val cardServiceViewModel: CardServiceViewModel by activityViewModels()
 
     private lateinit var binding: FragmentCardAnalysisBinding
 
@@ -18,6 +36,41 @@ class CardAnalysisFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentCardAnalysisBinding.inflate(layoutInflater, container, false)
 
+        binding.scanFab.setOnClickListener { scanCard() }
+
         return binding.root
+    }
+
+    private fun scanCard() {
+        setLoading(true)
+
+        val card = cardViewModel.cardLiveData.value ?: return
+
+        val handler = CoroutineExceptionHandler { _, throwable ->
+            setLoading(false)
+            Log.e(TAG, "Error adding card", throwable)
+
+            Snackbar.make(
+                binding.root,
+                "Could not find card due to a network error. Check your internet connection and try again.",
+                LENGTH_LONG
+            ).show()
+        }
+
+        lifecycleScope.launch(handler) {
+            val found = cardServiceViewModel.search(card).also { setLoading(false) }
+
+            val navController = findNavController()
+            navController.navigate(R.id.cardDetailFragment, Bundle().apply {
+                putString(ARG_CARD_ID, found.id)
+                putBoolean(ARG_HAS_CARD_FACES, found.cardFaces.isNotEmpty())
+            })
+
+            return@launch
+        }
+    }
+
+    private fun setLoading(loading: Boolean) {
+        binding.loadingProgressIndicator.isVisible = loading
     }
 }
